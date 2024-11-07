edced20e3db31ed4820e76d3a592d5acbc1cc544
ble1gh <brookpleigh@gmail.com>
2024-11-07 13:43:16 -0700

    created HW3 shell script

---
+++ Perceptron/HW3_shell.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Run standard_perceptron.py
+python3 standard_perceptron.py
+
+# Run averaged_perceptron.py
+python3 averaged_perceptron.py
+
+# Run voted_perceptron.py
+python3 voted_perceptron.py
